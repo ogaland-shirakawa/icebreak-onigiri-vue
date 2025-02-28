b784e9e79102220511d3a1739cca4ce50fd3a161
ogaland-shirakawa <shirakawa.makoto@ogaland.com>
2025-02-28 03:44:12 +0000

    とりあえず作った

---
+++ src/views/ThemeComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <v-card
+      class="mr-5 mt-15"
+      color="orangelighten-5"
+      width="1000"
+      height="500"
+    >
+      <p>{{ "お題" }}</p>
+      <div>
+        {{ "行きたい国は？" }}
+      </div>
+      <v-btn color="light-green darken-4">Start</v-btn>
+    </v-card>
+  </div>
+</template>
+<script>
+export default {};
+</script>
