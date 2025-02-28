8a95d5487569c224faa93f460c69e0fe8b5c93e6
ogaland-shirakawa <shirakawa.makoto@ogaland.com>
2025-02-28 03:50:17 +0000

    Move ThemeComponent and QuestionComponent to the correct branch

---
+++ src/views/QuestionComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <v-card class="d-flex" width="1000" height="500">
+      <p>{{ "回答者" }}</p>
+      <div>
+        {{ "行きたい国は？" }}
+      </div>
+      <v-btn>Start</v-btn>
+      <div>
+        <p>{{ "お題" }}</p>
+        <div>
+          {{ "行きたい国は？" }}
+        </div>
+        <v-btn>Start</v-btn>
+      </div>
+    </v-card>
+  </div>
+</template>
+<script>
+export default {};
+</script>
