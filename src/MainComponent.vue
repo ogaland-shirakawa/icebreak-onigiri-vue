6d464d044e4ff43dd2d10307ac0577e85b23dc17
ogaland-shirakawa <shirakawa.makoto@ogaland.com>
2025-02-28 03:43:41 +0000

    とりあえず作った

---
+++ src/MainComponent.vue
@@ -0,0 +1,30 @@
+<template color="orange">
+  <div id="app" width="1080" height="1080">
+    <!-- <v-app-bar app color="#F4E2D0">
+      <span class="white--text">
+        佐藤です、好きなおにぎりの具は梅干しです。
+      </span>
+    </v-app-bar> -->
+    <ThemeComponent />
+    <QuestionComponent />
+  </div>
+</template>
+
+<script>
+import ThemeComponent from "./views/ThemeComponent.vue";
+import QuestionComponent from "./views/QuestionComponent.vue";
+
+export default {
+  name: "MainComponent",
+  components: {
+    ThemeComponent,
+    QuestionComponent,
+  },
+};
+</script>
+
+<style>
+#add {
+  background-color: orange;
+}
+</style>
